e3762edc5186fe04ac1edc4c5f39797247b29895
jamaral33 <jpgamedev07@gmail.com>
2026-06-18 07:50:22 -0300

    testes no projeto

---
+++ PomoDeEris/PomoDeEris/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PomoDeEris.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
