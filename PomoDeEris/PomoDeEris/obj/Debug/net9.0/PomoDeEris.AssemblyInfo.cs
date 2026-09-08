92a5fb9fece27f60f148406da5987da088d6ac89
Joao Vitor Nunes <joaovitornunesdev@gmail.com>
2026-09-07 21:32:56 -0300

    correção de falhas no código

--- PomoDeEris/PomoDeEris/obj/Debug/net9.0/PomoDeEris.AssemblyInfo.cs
+++ PomoDeEris/PomoDeEris/obj/Debug/net9.0/PomoDeEris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PomoDeEris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb948b60fffbd47059e6048294e1fa302fcf42a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec486a710668afc24ec1c50f07eab4685b264bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("PomoDeEris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PomoDeEris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
